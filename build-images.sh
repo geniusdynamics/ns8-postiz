08e4dcc909269011d514f6ebbe0fce05218e63ae
Kemboi Elvis <kemboielvis22@gmail.com>
2026-02-08 01:49:43 +0300

    refactor: replaced all kickstart names with postiz

--- build-images.sh
+++ build-images.sh
@@ -18,17 +18,17 @@ reponame="postiz"
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-postiz container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-postiz; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-postiz -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-postiz \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
